ccceabd62b9c1993f50fde1e8ca763d1621cae7f
mamba <neelov.ev@mail.ru>
2023-10-02 14:03:41 +0300

    Фикс многопоточности: одновременное чтение и запись.

--- Database/MySqlDatabase.cs
+++ Database/MySqlDatabase.cs
@@ -1,67 +1,53 @@
 ﻿using System.Data;
 using MySql.Data.MySqlClient;
 
-namespace Database;
-
-public class MySqlDatabase
+namespace Database
 {
-    private MySqlConnection Connection;
-    private string ConnectionString;
+    public class MySqlDatabase
+    {
+        private string ConnectionString;
 
-    public MySqlDatabase(string server, string database, string username, string password)
-    {
-        ConnectionString = $"Server={server};Database={database};User ID={username};Password={password};";
-        Connection = new MySqlConnection(ConnectionString);
-    }
-
-    private async Task ConnectionOpenAsync()
-    {
-        if (Connection.State is not ConnectionState.Open)
-            await Connection.OpenAsync();
-    }
-    
-    private async Task ConnectionCloseAsync()
-    {
-        if (Connection.State is not ConnectionState.Closed)
-            await Connection.CloseAsync();
-    }
-    
-    private MySqlCommand CreateCommand() => Connection.CreateCommand();
-    
-    public async Task ExecuteNonQueryAsync(string query, params MySqlParameter[] parameters)
-    {
-        await ConnectionOpenAsync();
-        await using var cmd = CreateCommand();
-        cmd.CommandText = query;
-        cmd.Parameters.AddRange(parameters);
-        cmd.ExecuteNonQuery();
-        await ConnectionCloseAsync();
-    }
-    
-    public async Task<MySqlDataReader> ExecuteQueryAsync(string query, params MySqlParameter[] parameters)
-    {
-        await ConnectionOpenAsync();
-        var cmd = CreateCommand();
-        cmd.CommandText = query;
-        cmd.Parameters.AddRange(parameters);
-        var reader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
-        return reader;
-    }
-    
-    public async Task<object> ExecuteScalarAsync(string query, params MySqlParameter[] parameters)
-    {
-        try
+        public MySqlDatabase(string server, string database, string username, string password)
         {
-            await ConnectionOpenAsync();
-            await using var cmd = CreateCommand();
+            ConnectionString = $"Server={server};Database={database};User ID={username};Password={password};";
+        }
+
+        public async Task ExecuteNonQueryAsync(string query, params MySqlParameter[] parameters)
+        {
+            await using MySqlConnection connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+
+            await using MySqlCommand cmd = connection.CreateCommand();
             cmd.CommandText = query;
             cmd.Parameters.AddRange(parameters);
+            
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task<MySqlDataReader> ExecuteQueryAsync(string query, params MySqlParameter[] parameters)
+        {
+            await using MySqlConnection connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+
+            await using MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            cmd.Parameters.AddRange(parameters);
+            
+            var reader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+            return reader;
+        }
+
+        public async Task<object> ExecuteScalarAsync(string query, params MySqlParameter[] parameters)
+        {
+            await using MySqlConnection connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+
+            await using MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            cmd.Parameters.AddRange(parameters);
+            
             var result = await cmd.ExecuteScalarAsync();
             return result;
-        }
-        finally
-        {
-            await ConnectionCloseAsync();
         }
     }
 }
