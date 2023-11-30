b5e94a59304a4f04b615cb1e4f725d026a6881e6
Neelov Eugene <neelov.ev@mail.ru>
2023-11-30 11:16:56 +0300

    Rename project

---
+++ MDatabase/MDatabase.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MDatabase;
+
+public class MDatabase
+{
+    private readonly string ConnectionString;
+
+    public MDatabase(string server, string database, string username, string password)
+    {
+        ConnectionString = $"Server={server};Database={database};User ID={username};Password={password};";
+    }
+
+    public async Task ExecuteNonQueryAsync(string query, params MySqlParameter[] parameters)
+    {
+        await using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+
+        await using var cmd = connection.CreateCommand();
+        cmd.CommandText = query;
+        cmd.Parameters.AddRange(parameters);
+
+        await cmd.ExecuteNonQueryAsync();
+    }
+
+    public async Task<MySqlDataReader> ExecuteQueryAsync(string query, params MySqlParameter[] parameters)
+    {
+        var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+
+        await using var cmd = connection.CreateCommand();
+        cmd.CommandText = query;
+        cmd.Parameters.AddRange(parameters);
+
+        var reader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+        return reader;
+    }
+
+    public async Task<object?> ExecuteScalarAsync(string query, params MySqlParameter[] parameters)
+    {
+        await using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+
+        await using var cmd = connection.CreateCommand();
+        cmd.CommandText = query;
+        cmd.Parameters.AddRange(parameters);
+
+        return await cmd.ExecuteScalarAsync();
+    }
+
+    public async Task<bool> PingAsync()
+    {
+        var connection = new MySqlConnection(ConnectionString);
+        
+        try
+        {
+            await connection.OpenAsync();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            return false;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+    }
+}
